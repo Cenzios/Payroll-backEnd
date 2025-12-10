93334c7f0db4d793cb12c9a2ddb4ef02762d5840
Pathum Rathnayaka <thilinapathumrathnayaka@gmail.com>
2025-12-10 13:23:44 +0530

    login done

--- src/routes/subscription.routes.ts
+++ src/routes/subscription.routes.ts
@@ -1,13 +1,18 @@
+// src/routes/subscription.routes.ts
 import express from 'express';
 import * as subscriptionController from '../controllers/subscription.controller';
 import { protect } from '../middlewares/authMiddleware';
 
 const router = express.Router();
 
+// ✅ PUT THIS LINE **BEFORE** router.use(protect)
+router.post('/subscribe', subscriptionController.subscribePlan);
+
+// NOW apply protection to routes below
 router.use(protect);
 
+// These routes are protected (need login)
 router.put('/upgrade', subscriptionController.upgrade);
 router.post('/addon', subscriptionController.addAddon);
-router.post('/subscribe', subscriptionController.subscribePlan);
 
 export default router;
