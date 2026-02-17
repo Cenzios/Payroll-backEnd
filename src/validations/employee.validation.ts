6e6e15f89a5dbcbbdff33f0bbf7b208a4a056a88
Pathum Rathnayaka <thilinapathumrathnayaka@gmail.com>
2026-02-17 12:36:52 +0530

    fixed 1 validations error

--- src/validations/employee.validation.ts
+++ src/validations/employee.validation.ts
@@ -2,13 +2,13 @@ import { body } from 'express-validator';
 
 const createEmployeeValidation = [
     body('fullName').notEmpty().withMessage('Full Name is required'),
-    body('address').notEmpty().withMessage('Address is required'),
+    body('address').optional({ values: 'falsy' }).trim(),
     body('accountNumber').optional(),
     body('nic').default('PENDING'),
     body('employeeId').notEmpty().withMessage('Employee ID is required'),
     body('contactNumber').notEmpty().withMessage('Contact Number is required'),
     body('joinedDate').isISO8601().toDate().withMessage('Valid Joined Date is required'),
-    body('designation').notEmpty().withMessage('Designation is required'),
+    body('designation').optional({ values: 'falsy' }).trim(),
     body('department').default('General'),
     body('email').optional({ values: 'falsy' }).trim().isEmail().withMessage('Valid Email is required'),
     body('dailyRate').isFloat({ min: 0 }).withMessage('Daily Rate must be positive'),
@@ -18,6 +18,8 @@ const updateEmployeeValidation = [
     body('fullName').optional().notEmpty(),
     body('joinedDate').optional().isISO8601().toDate(),
     body('dailyRate').optional().isFloat({ min: 0 }),
+    body('designation').optional({ values: 'falsy' }).trim(),
+    body('address').optional({ values: 'falsy' }).trim(),
     body('email').optional({ values: 'falsy' }).trim().isEmail().withMessage('Valid Email is required'),
 ];
 
